658efc848e62f35c4fc8cb1376f5e1659e232f88
mpyw <ryosuke_i_628@yahoo.co.jp>
2023-07-06 23:47:48 +0900

    refactor: 💡 Rename in examples: models -> entities

--- example/infra/db/db.go
+++ example/infra/db/db.go
@@ -1,4 +1,4 @@
 package db
 
 //go:generate docker compose run --rm -T sqlc generate
-//go:generate sqlc-restruct separate-interface --models-file-name=models.gen.go --querier-file-name=querier.gen.go --iface-dir=../../domain/repos --iface-pkg-name=repos --iface-pkg-url=github.com/mpyw/sqlc-restruct/example/domain/repos --models-dir=../../domain/models --models-pkg-name=models --models-pkg-url=github.com/mpyw/sqlc-restruct/example/domain/models
+//go:generate sqlc-restruct separate-interface --models-file-name=entities.gen.go --querier-file-name=querier.gen.go --iface-dir=../../domain/repos --iface-pkg-name=repos --iface-pkg-url=github.com/mpyw/sqlc-restruct/example/domain/repos --models-dir=../../domain/entities --models-pkg-name=entities --models-pkg-url=github.com/mpyw/sqlc-restruct/example/domain/entities
